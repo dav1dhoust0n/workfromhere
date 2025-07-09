1f05743a4ca5caa840293e27394947e93ce61d3d
David Houston <davidaehouston@gmail.com>
2025-07-09 20:50:03 +0100

    Adding controller calls for CRUD ops (#3)

--- backend/src/main/java/dev/dav1dhoust0n/workfromhere/spaces/controller/SpaceResourceController.java
+++ backend/src/main/java/dev/dav1dhoust0n/workfromhere/spaces/controller/SpaceResourceController.java
@@ -1,7 +1,56 @@
 package dev.dav1dhoust0n.workfromhere.spaces.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import dev.dav1dhoust0n.workfromhere.spaces.model.SpaceResource;
+import dev.dav1dhoust0n.workfromhere.spaces.service.SpaceResourceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class SpaceResourceController {
+
+    // Hard-Coded for now, needs to put inside an env file
+    private final String API_VERSION = "1";
+    private final String SPACES_URI = "/api/v" + API_VERSION + "/spaces/";
+
+    private final SpaceResourceService spaceResourceService;
+
+    public SpaceResourceController(SpaceResourceService spaceResourceService) {
+        this.spaceResourceService = spaceResourceService;
+    }
+
+    @GetMapping(value = SPACES_URI)
+    public List<SpaceResource> getAllSpaces() {
+        return ResponseEntity
+                .ok(spaceResourceService.getAllSpaces());
+    }
+
+    @GetMapping(value = SPACES_URI + "{id}")
+    public ResponseEntity<SpaceResource> getSpaceById(@RequestParam long id) {
+        return ResponseEntity
+                .ok(spaceResourceService.getSpaceById(id));
+    }
+
+    @PostMapping(value = SPACES_URI)
+    public ResponseEntity<SpaceResource> createSpace(@RequestBody SpaceResource spaceResource) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(spaceResourceService.createSpace(spaceResource));
+    }
+
+    @PutMapping(value = SPACES_URI + "{id}")
+    public ResponseEntity<Void> updateSpaceById(@RequestBody SpaceResource updatedSpace, @PathVariable long id) {
+        spaceResourceService.updateSpace(updatedSpace, id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = SPACES_URI + "{id}")
+    public ResponseEntity<Void> deleteSpaceById(@RequestParam long id) {
+        spaceResourceService.deleteSpace(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
