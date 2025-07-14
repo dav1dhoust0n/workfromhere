ea076a0a1adb5b353b885d6c95378fba6d0c6f24
David Houston <davidaehouston@gmail.com>
2025-07-14 10:10:35 +0100

    Adding Spaces Excveptions & global exception handler

---
+++ backend/src/main/java/dev/dav1dhoust0n/workfromhere/spaces/exception/SpaceResourceException.java
@@ -0,0 +1,8 @@
+package dev.dav1dhoust0n.workfromhere.spaces.exception;
+
+public class SpaceResourceException  extends RuntimeException {
+
+    public SpaceResourceException(String message) {
+        super(message);
+    }
+}
